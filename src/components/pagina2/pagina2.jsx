24ba397805fd48f6fef4cafbdda041bf2d41699f
Lidiador <alex.ai.conejo@gmail.com>
2024-12-18 17:34:22 -0300

    router config

---
+++ src/components/pagina2/pagina2.jsx
@@ -0,0 +1,12 @@
+
+function Pagina2() {
+const nombre = "codebroxs";
+  return (
+    <>
+    <h1>pagina 2</h1>
+    <h3>{nombre}</h3>
+    </>
+  )
+}
+
+export default Pagina2
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+import Root from "./components/routes/Root.jsx";
+import {
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Pagina2 from './components/pagina2/pagina2.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App";
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    children:[
+      { path:"/", element:<App/>},
+      { path:"/pagina2", element:<Pagina2/>},
+
+    
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
